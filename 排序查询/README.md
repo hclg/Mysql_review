651a5fa9f6596b51b50a7045139d76f370deafdb
hclg <2249121951@qq.com>
2020-03-01 17:12:18 +0800

    数据库

--- 排序查询/README.md
+++ 排序查询/README.md
@@ -1,1 +1,11 @@
-# 
+#  排序查询
+
+* 语法
+```mysql
+/*
+select 查询列表
+from 表
+order by 排序 列表 asc|desc
+*/
+
+```
--- 排序查询/排序查询.sql
+++ 排序查询/排序查询.sql
@@ -2,3 +2,15 @@ use test;
 show  tables;
 # 排序查询
 SELECT * FROM emp;
+/*
+select 查询列表
+from 表
+[where  筛选条件]
+order by 排序 列表 asc|desc
+*/
+
+SELECT * from emp ORDER by cs;
+# 查询大于等于90的信息 从小到大
+SELECT * from  emp 
+where dep_id BETWEEN 90 and 100 
+ORDER by sc desc;
