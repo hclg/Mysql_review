c493ea56b9d84384764e2b474d4e6f5ac3c1e32d
hclg <2249121951@qq.com>
2020-03-01 14:53:06 +0800

    创建排序查询sql

---
+++ 排序查询/HCLG Session.sql
@@ -0,0 +1,6 @@
+INSERT INTO `test`.`hcl` (id, name)
+VALUES
+  (id:int, 'name:varchar');INSERT INTO `test`.`sc` (cid, grade, sid)
+  VALUES
+    (cid:int, grade:int, sid:int);
+
--- 排序查询/排序查询.sql
+++ 排序查询/排序查询.sql
@@ -1,2 +1,4 @@
 use test;
 show  tables;
+# 排序查询
+SELECT * FROM emp;
